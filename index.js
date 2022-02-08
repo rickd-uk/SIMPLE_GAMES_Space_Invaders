9c4e5d58e3a40b0910a6f396a32c792ebb9e0681
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-02-08 11:39:30 +0900

    redraw on resize

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+;(() => {
+	const canvas = document.querySelector('canvas')
+	const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+	canvas.width = innerWidth
+	canvas.height = innerHeight
 
-class Player {
-	constructor() {
-		this.velocity = {
-			x: 0,
-			y: 0,
+	class Player {
+		constructor() {
+			this.velocity = {
+				x: 0,
+				y: 0,
+			}
+
+			const image = new Image()
+			image.src = './img/spaceship.png'
+			image.onload = () => {
+				const scale = 0.15
+				this.image = image
+				this.width = image.width * scale
+				this.height = image.height * scale
+				this.setPosition()
+			}
 		}
 
-		const image = new Image()
-		image.src = './img/spaceship.png'
-		image.onload = () => {
-			const scale = 0.15
-			this.image = image
-			this.width = image.width * scale
-			this.height = image.height * scale
+		setPosition() {
 			this.position = {
 				x: canvas.width / 2 - this.width / 2,
 				y: canvas.height - this.height - 20,
 			}
 		}
+
+		redrawOnResize() {
+			canvas.width = innerWidth
+			canvas.height = innerHeight
+			this.setPosition()
+		}
+
+		draw() {
+			// c.fillStyle = 'red'
+			// c.fillRect(this.position.x, this.position.y, this.width, this.height)
+			if (this.image) c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+		}
 	}
 
-	draw() {
-		// c.fillStyle = 'red'
-		// c.fillRect(this.position.x, this.position.y, this.width, this.height)
-		if (this.image) c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-	}
-}
-
-const player = new Player()
-player.draw()
-
-function animate() {
-	requestAnimationFrame(animate)
-	c.fillStyle = 'black'
-	c.fillRect(0, 0, canvas.width, canvas.height)
+	const player = new Player()
 	player.draw()
-}
 
-animate()
+	function animate() {
+		requestAnimationFrame(animate)
+		c.fillStyle = 'black'
+		c.fillRect(0, 0, canvas.width, canvas.height)
+		player.draw()
+	}
+
+	animate()
+
+	addEventListener('resize', () => {
+		player.redrawOnResize()
+	})
+
+	addEventListener('keydown', ({ key }) => {
+		switch (key) {
+			case 'a':
+				console.log('left')
+				break
+			case 'd':
+				console.log('right')
+				break
+		}
+	})
+})()
