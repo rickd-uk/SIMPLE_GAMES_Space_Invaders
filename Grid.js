760eeee27a7dbe64ccfc9830e1277e45d6ae8edc
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-02-08 18:14:00 +0900

    spawn grids at intervals

--- Grid.js
+++ Grid.js
@@ -29,7 +29,6 @@ class Grid {
 					}),
 				)
 			}
-			console.log(this.invaders)
 		}
 	}
 	update(canvas) {
--- index.js
+++ index.js
@@ -12,13 +12,16 @@ import Controls from './Controls.js'
 	canvas.height = innerHeight
 
 	const player = new Player(canvas)
-	const grids = [new Grid(canvas)]
+	const grids = []
 	const projectiles = []
 	const controls = new Controls(player, projectiles)
 
 	addEventListener('resize', () => {
 		player.redrawOnResize(canvas)
 	})
+
+	let frames = 0
+	let randomInterval = Math.floor(Math.random() * 500 + 500)
 
 	function animate() {
 		requestAnimationFrame(animate)
@@ -43,6 +46,16 @@ import Controls from './Controls.js'
 				invader.update(c, { velocity: grid.velocity })
 			})
 		})
+
+		// Spawing enemies
+		if (frames % randomInterval === 0) {
+			// Divisible by 1000
+			grids.push(new Grid(canvas))
+			randomInterval = Math.floor(Math.random() * 500 + 500)
+			frames = 0
+		}
+
+		frames++
 	}
 
 	animate()
